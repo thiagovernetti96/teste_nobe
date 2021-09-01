31037dc7b80a4e033d690222516bb25b48fc3134
THIAGO V <thaigovernetti@gmail.com>
2021-09-01 20:30:56 -0300

    model client gerado e simple form configurado

---
+++ app/models/client.rb
@@ -0,0 +1,24 @@
+class Client < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :client_number, presence: true, uniqueness: true
+    validates_associated :content
+
+    has_many :bank_accounts
+
+    before_save :format_name
+
+    def format_name
+        self.first_name = self.first_name.upcase
+        self.last_name = self.first_name.upcase
+    end
+
+    def num_accounts
+        self.bank_accounts.count
+    end
+
+    def to_s
+        "#{first_name} #{middle_name} #{last_name}"
+    end
+
+end
