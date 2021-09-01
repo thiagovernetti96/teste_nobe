e15204b281425cc4254b26b46061139d23dc9ab3
THIAGO V <thaigovernetti@gmail.com>
2021-09-01 20:42:58 -0300

    model BankAccount criado

---
+++ app/models/bank_account.rb
@@ -0,0 +1,22 @@
+class BankAccount < ApplicationRecord
+  belongs_to :client
+
+  validates :client, presence: true 
+  validates :account_number, presence: true, uniqueness: true
+  validates :balance, presence: true, numericality: true
+
+  before_validation :load_defaults
+
+  has_many :account_transactions
+
+  def to_s
+    account_number
+  end
+
+  def load_defaults
+    if self.new_record?
+      self.balance = 0.00
+    end
+  end
+
+end
